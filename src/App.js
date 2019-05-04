614104e0f514dd086e576d9b1846c12a47a111e2
Justin <jdlow2@gmail.com>
2019-05-04 13:18:13 -0400

    adds Query component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const ExchangeRates = () => (
+  <Query
+    query={gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>loading</p>;
+      if (error) {
+        console.log(error.message);
+
+        return <p>error</p>;
+      }
+      return data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+          <p>
+            {currency}: {rate}
+          </p>
+        </div>
+      ));
+    }}
+  </Query>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>my first apollo app 🚀</h2>
+        <ExchangeRates />
       </header>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ApolloClient, { gql } from "apollo-boost";
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
-})
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
 
-client.query({
-  query: gql`
-  {
-    rates(currency: "USD") {
-      currency
-    }
-  }
-  `
-}).then(result => console.log(result));
+// client
+//   .query({
+//     query: gql`
+//       {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `,
+//   })
+//   .then(result => console.log(result));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
